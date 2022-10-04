e0cb83f0e60d5cd93bd0f50ad272f38b43ace356
Hidemaru-Jupiter <65824185+Hidemaru-Jupiter@users.noreply.github.com>
2022-10-05 00:41:20 +0900

    Add files via upload

--- html/test.php
+++ html/test.php
@@ -3,14 +3,27 @@
     <title>niconicoMirror</title>
 </head>
 
-<video id="video1" width="100%" height="25%" preload="auto" controls autoplay></video>
+<script src="DPlayer.min.js"></script>
+<div id="dplayer" style="width:100%;height:25%;"></div>
+<script>
+    const dp = new DPlayer({
+        container: document.getElementById('dplayer'),
+        screenshot: true,
+        video: {
+            thumbnails:''
+        },
+        danmaku:{
+            id:"test",
+        },
+    });
+</script>
 <table>
     <tr>
         <th style="width:20%;height:10%">
-            <button onclick="video_elem.requestFullscreen();" style="width:100%;height:100%;" >全画面モード</button>
+            <button onclick="dp.video.requestFullscreen();" style="width:100%;height:100%;" >全画面モード</button>
         </th>
         <th style="width:1%;height:10%">
-            <input type="checkbox" id="auto">
+            <input type="checkbox" id="auto" checked>
         </th>
         
         <th style="width:80%;height:10%">
@@ -21,7 +34,6 @@
 
 <script type="text/javascript">
     let playing_id = 0;
-    let video_elem = document.getElementById("video1");
     let dialog = document.getElementById("dialog");
     
     //cookie値を連想配列として取得する
@@ -46,32 +58,38 @@
         }else{
             let dir = id_elem.innerText.replace("\n","");
             let uri = ".";
-            dir.split("/").forEach((element)=>{
-                uri += "/" + encodeURIComponent(element);
-                document.getElementById("console").innerText = element;
-            });
+            uri_dir = "./" + encodeURIComponent(dir.split("/")[0]) + "/";
+            document.getElementById("console").innerText = dir.split("/")[1];
             cookieArray = getCookieArray();
-            video_elem.src = uri;
+            dp.switchVideo(
+                {
+                    url: uri_dir + encodeURIComponent('original.mp4'),
+                    pic: uri_dir + encodeURIComponent('thumbnail.jpg'),
+                    thumbnails: uri_dir + encodeURIComponent('hconcat100_thumbnail.jpg'),
+                },
+                {
+                    id: dir.split("/")[1],
+                    api: uri_dir + 'comment.txt'
+                }
+            );
             if (typeof cookieArray[encodeURIComponent(dir)] !== 'undefined'){
-                video_elem.currentTime = Number(cookieArray[encodeURIComponent(dir)]);
+                dp.video.currentTime = Number(cookieArray[encodeURIComponent(dir)]);
             }
+            dp.video.play();
             return true;
         }
     }
-    video_elem.addEventListener('timeupdate', function() {
+    dp.video.addEventListener('timeupdate', function() {
         let dir = document.getElementById("f_"+playing_id).innerText.replace("\n","");
-        document.cookie = encodeURIComponent(dir) + "=" + video_elem.currentTime + ";max-age=" + 60*60*24*7;
-        if (video_elem.duration-0.5 <= video_elem.currentTime){
+        document.cookie = encodeURIComponent(dir) + "=" + dp.video.currentTime + ";max-age=" + 60*60*24*7;
+        if (dp.video.duration-0.5 <= dp.video.currentTime){
             if(document.getElementById("auto").checked){
-                video_elem.currentTime = 0;
                 if(!change(Number(playing_id)+1)){
-                    video_elem.src = null;
+                    dp.video.currentTime = 0;
                 }
             }
         }
     });
-    
-    
 </script>
 <div style="width:100%;height:70%;overflow-y:scroll;">
 <?php
@@ -80,18 +98,19 @@ $dirList = glob('*',GLOB_ONLYDIR);
 echo '<table>';
     $counter = 0;
     foreach($dirList as $dirname){
-        // フォルダの寿命確認-------------------------------------
         if(!strcmp($dirname[0], "_")){
+            continue;
+        }
+        // フォルダの寿命確認-------------------------------------
             if(!file_exists($dirname.'/create.txt')){
                 file_put_contents($dirname.'/create.txt', time());
             }
             $unit_time = (int)file_get_contents($dirname.'/create.txt');				
-            $alive_time = $unit_time + (1 * 24 * 60 * 60) - time(); // 1週間
+            $alive_time = $unit_time + (1 * 24 * 60 * 60) - time(); // 1日
             if(0 > $alive_time){
                 system("rm -rf {$dirname}");
                 continue;
             }
-        }
         // ----------------------------------------------------
         echo '<tr style="background-color:black;">';
             if (file_exists($dirname.'/thumbnail.jpg')){
@@ -108,11 +127,9 @@ echo '<table>';
                 foreach($contentsList as $filename){
                     if (preg_match('/\.(avi|flv|mkv|mov|mp4|webm)/', $filename) == 1){
                         $s_filename = explode('/', $filename);
-                        if (!($s_filename[1] == 'original.mp4' || $s_filename[1] == 'paint.mp4')){
-                            echo '<button id="f_'.$counter.'" style="width:100%;height:100px;" onclick="change(\''.$counter
-                            .'\');"><span style="font-size:1px;">'.$s_filename[0].'/</span><br/><span style="font-size:24px;">'.$s_filename[1].'</span></button>';
-                            $counter += 1;
-                        }
+                        echo '<button id="f_'.$counter.'" style="width:100%;height:100px;" onclick="change(\''.$counter
+                        .'\');"><span style="font-size:1px;">'.$s_filename[0].'/</span><br/><span style="font-size:24px;">'.$s_filename[1].'</span></button>';
+                        $counter += 1;
                     }
                 }
                 echo '</div></th>';
